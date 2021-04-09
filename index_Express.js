c3e5a22b91e0c1be6216303374b152c989a52c3c
Kunal Dixit <kunaldixit@Kunals-MacBook-Air.local>
2021-04-09 09:56:59 +0800

    update

--- index_Express.js
+++ index_Express.js
@@ -27,14 +27,14 @@ const app = express();
 /*
 Todo: Whenever we have any request coming up/ being received (http),the http request always comes up in the form of a simple text. HTTP request is not javascript object and neither it is particualar to any specific language. Instead it is simple text info.
 
-Todo: But the beauty of Express is that it can parse it and auto creates 2 objects (and thier associated properties and methods) which are automatically be passed as arguements in app.use()
+Todo: But the beauty of Express is that it can parse it and auto creates 2 objects (and thier associated properties and methods) which are automatically passed as arguements in app.use()
 
 Todo: Generally these 2 OBJECTS are named as req, res . req as requests and this represents INCOMING request . res for response and this represents OUTGOING response
 */
 
 /*
 
-* res.send() : is an important method of OBJECT res (req and res are objects created by the Express framework based on the HTTP request received from the clients end).
+* res.send() : is an important method of object 'res' (req and res are objects created by the Express framework based on the HTTP request received from the clients end).
 * It sends back the HTTP response to the client
 * The body parameter can be a buffer object, a string, an Object, * Boolean or an Array
 * e.g.
@@ -52,21 +52,21 @@ Todo: Generally these 2 OBJECTS are named as req, res . req as requests and this
 
 */
 /*
-* NOTE: imp tip: whenever you make any change in the js file...make sure to re run the file in node terminal */
+* NOTE: imp tip: whenever you make any change in the js file...make sure to re run the file in node terminal, unless you executing the file through 'nodemon' after npm installing nodemon */
 /*
- * Also, we can either have app.use((req,res)=>{}) or app.get('path', callbackFunc)
+ * Also, we can either have app.use((req,res)=>{}) or app.get('path', callbackFunc). Using both may give us an error
 *
-There can only be one res.send() because once you have sent something back to the clien, that process is taken careOff. If you have multiple res s, then it leads to error so only one res.send()
+There can only be one res.send() because once you have sent something back to the client, the connection terminates for that request, and that process is taken careOff. If you have multiple res's, then it leads to error so only one res.send()
 
 /*
 * Express Routing basics:
 
-How to respond with different content based on different requests:
+* How to respond with different content based on different requests:
 
-Routing is not only particular to Express but is a term used in all server creation and management
+* Routing is not only particular to Express but is a term used in all server creation and management
 
-Routing is basically understanding incoming requests and the path that has been requested by the client and then accordingly matching that to some code or response.
-
+* Routing is basically understanding incoming requests and the path that has been requested by the client and then accordingly matching that to some code or response.
+* Routing is important to understand as your responses certainly will differ basis the route/s sought by the client.
 */
 
 //  Lets start by adding simple routes:
@@ -78,7 +78,7 @@ Routing is basically understanding incoming requests and the path that has been 
 // * GET REQUEST
 // **********************************************************
 /*
-*app.get() exoects two parameters , 1st the path that we are matching e.g. "/cats" and then the callback function  - absolutely the same as used with app.use(req,res) with the logic that this callback function will run when the incoming requests matches the stated mentioned path(1st parameter)
+*app.get() exoects two parameters , 1st the path that we are matching e.g. "/cats" and then the callback function (which is absolutely the same as used with app.use(req,res) with the logic that this callback function will run when the incoming requests matches the stated mentioned path(1st parameter))
 */
 
 // todo : Example of a cat's route
@@ -104,19 +104,32 @@ app.get('/', (req, res) => {
 
 /*
 todo: EXPRESS PATH PARAMETERS:
-We cant really specify each path for each of the page as there can be multiple pages in a website so instead we define a pattern, thus we work on a better mechanism of defining a variable path parameter
-*/
-/* in below, '/r/:' is pattern the server will search for when the requests is received(especially anything that comes after ":"). localhost:8080/r/cat and localhost:8080/r/dogs will get accounted for and will be considered but localhost:8080/cat and localhost:8080/dog will not be considered
+* We cant really specify each path for each of the page as there can be multiple pages in a website so instead we define a pattern, thus we work on a better mechanism of defining a variable path parameter
+* in below, '/r/:' is pattern the server will search for when the requests is received(especially anything that comes after ":"). localhost:8080/r/cat and localhost:8080/r/dogs will get accounted for and will be considered BUT localhost:8080/cat and localhost:8080/dog will not be considered because they are dont have '/r/' pattern.
 
-so in short, anything that follows /r/: will be considered
-Remember the importance of ":"
+* so in short, the exact pattern that follows '/r/:' will be considered. Remember the importance of ":"
 */
+
 app.get('/r/:subreddit', (req, res) => {
   console.log('This is a PARAM in Action');
   res.send('THIS is Express Path Params in ACTION');
 });
 
-// todo: incase you would like to put a customised message for if you recieve a get Request with invalid path that doesnt exists in your website, use "*"....BUT make a point to never mention the below code in the beginning because "*" matches all the path patterns ,,,thus all paths written after the below code will be ignored by Express..so ALWAYS keep the below code as the last option in the sequence:
+app.get('/s/:searchVar', (req, res) => {
+  const { searchVar } = req.params;
+  const { count, color } = req.query;
+  if (!count) {
+    res.send('Nothing found if nothing searched');
+  } else {
+    console.log(req.query);
+    console.log(`The client as mentioned ${searchVar} as the Path Parameter`);
+    console.log(`The query selector specified by client are count as ${count} and color ${color}`);
+    // res.send(`The client as mentioned ${searchVar} as the Path Parameters`);
+    res.send(`The query selector specified by client are count as ${count} and color ${color}`);
+  }
+});
+
+// todo: (Final catch 'get' (wild card character '*') - incase you would like to put a customised message for if you recieve a get 'Request' with an invalid path that doesnt exists in your website, use "*"....BUT make a point to never mention the below code in the beginning because "*" matches all the path patterns ,,,thus all paths written after the below code will be ignored by Express..so ALWAYS keep the below code as the last option in the sequence:
 
 // app.get('*', (req, res) => {
 //   // console.log('Customised msg for INVALID path');
@@ -124,7 +137,7 @@ app.get('/r/:subreddit', (req, res) => {
 // });
 
 // **********************************************************
-// * POST REQUEST  - USE POSTMAN to see the post request and response in execution
+// * POST REQUEST  - USE POSTMAN to observe the post request and response in execution
 // **********************************************************
 
 app.post('/cats', (req, res) => {
@@ -166,15 +179,42 @@ and thats the whole purpose of ports existance. You can have different servers r
 /*
  *  Chapter: req.param:
 
-* This property is (GIVES) an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available as req.params.name. This object defaults to {}.
+* This property is (GIVES) an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available through req.params.name. This object defaults to {}.
 
-It tells you (in an Object form), what route path has been mentioned and captures it therof in an Object form
+* It tells you (in an Object form), what route path has been mentioned by the client and captures it theroff in an Object form
 
  */
+//* Here, paramVar is a a sort of variable and captures anything that gets passed in the pattern :(oneWord)
+// * If client requests for  /c/:paramVar/var2 : this is a different pattern since there is word/var 'var2' after ":"
 
-app.get('/c/:paramVar', (req, res) => {
-  console.dir(req.params);
-  console.log(req.params);
+//  In ghe below example, we have two variables being tracked and both the variables with prefixed ":" are defining a certain pattern
+
+app.get('/c/:paramVar/:postId', (req, res) => {
+  const { paramVar, postId } = req.params;
+
+  console.log(paramVar);
   console.log('This is req.param example');
-  res.send('This is a req.param example');
+  res.send(`The client requested for postID ${postId} on the page ${paramVar}`);
+});
+
+// * **************
+/*
+* Query String is the portion of the URL wich starts with "?" and client can include and pass on the information in this query String to back server. The information is commonly stored in the form of key value pairs
+* Often applications are setup that they are expecting something in the query string
+* e.g. if you search something on MDN pages say colors, ...then you'd be able to see the query string "?q=colors" in the URL
+
+* the "q" is just the name of the query string
+
+* If you modify your search to search for Node, ..then the query String changes to "?q=node"
+
+todo : Its very important to track query string (aka key value pairs) so that the info which the client has requested can be searched in the database and relevant info can be processed and provided back to the client
+
+* We dont need to track query string by specifying a certain pattern in the get("path",callback(req,res)) but instead there is a dedicated property available in 'req' object called as req.query
+*/
+
+app.get('/r/searchVar', (req, res) => {
+  const { searchVar } = req.params;
+  console.log(req.query);
+  console.log(`The client as mentioned ${searchVar} as the query Selector`);
+  res.send(`The client as mentioned ${search} as the query Selector`);
 });
